824096f3b4f857ba99ff817b144b8f1ea375c69c
zhouqianyu <285942677@qq.com>
2019-05-19 03:26:24 +0800

    add func of file upload

--- src/main/java/com/chatting/controller/MessageController.java
+++ src/main/java/com/chatting/controller/MessageController.java
@@ -10,9 +10,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.commons.CommonsMultipartFile;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
+import java.io.File;
+import java.io.IOException;
+import java.util.Date;
 import java.util.List;
 
 @Controller
@@ -59,5 +63,12 @@ public class MessageController {
             return responseData.successed("");
         }else
             return responseData.unKnowError();
+    }
+    @RequestMapping(value = "/recognize", method = RequestMethod.POST)
+    public String recognize(@RequestParam("file") CommonsMultipartFile file) throws IOException {
+        String path="/var/www/record/"+new Date().getTime()+file.getOriginalFilename();
+        File newFile = new File(path);
+        file.transferTo(newFile);
+        return responseData.successed("语音转文字");
     }
 }
